157598d879f949818f41acf538f55a36f36509e9
Ángel Píñar Rivas <anglepiri22@gmail.com>
2021-11-02 22:25:11 +0100

    ADD ship creation test #15

--- universe/universe_test.go
+++ universe/universe_test.go
@@ -9,11 +9,28 @@ import (
 func TestUniverseInitialization(t *testing.T){
 	u:=CreateUniverse()
 
+	assert.NotNil(t, u)
 	assert.NotNil(t, u.sectors[0])
 }
 
 func TestSectorsAreConnected(t *testing.T){
-	u:=CreateUniverse()
+	u := CreateUniverse()
 
-	assert.NotNil(t, u.sectors[0].neighbourhood[0])
+	for i:=0 ; i<TotalSectors ; i++ {
+		assert.Greater(t, len(u.sectors[i].neighbourhood), 0)
+	}
+}
+
+func TestShipCreation(t *testing.T){
+	u := CreateUniverse()
+
+	for i:=0 ; i<TotalSectors ; i++ {
+		var shipCreated, shipURL = CreateNewShip(u, "someShipName")
+		assert.True(t, shipCreated)
+		assert.Contains(t, shipURL, "/")
+	}
+	
+	var shipCreated, shipURL = CreateNewShip(u, "someShipName")
+	assert.False(t, shipCreated)
+	assert.Empty(t, shipURL)
 }
