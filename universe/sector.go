2771c314dce6b0ea9e1bcb2f9d32268bd1d046ca
Ángel Píñar Rivas <angle@correo.ugr.es>
2021-11-09 19:26:27 +0100

    #18 CREATE changeSector method to allow players to move between sectors

--- universe/sector.go
+++ universe/sector.go
@@ -32,8 +32,24 @@ func (sector *Sector) ShotWeapons(attacker ship.Ship, victim ship.Ship) {
 
 }
 
-func (sector *Sector) ChangeSector(traveling ship.Ship, destinationSectorName string) {
+func (sector *Sector) ChangeSector(travelingShip ship.Ship, destinationSectorName string) {
+	sectorExists := false
+	var neighbour Sector
+	for i:=0 ; i<len(sector.neighbourhood) && !sectorExists ; i++{
+		sectorExists = sector.neighbourhood[i].name == destinationSectorName
+		if sectorExists {
+			neighbour = sector.neighbourhood[i]
+		}
+	}
+	
+	if sectorExists {
+		sector.RemoveShip(travelingShip)
+		neighbour.AddExistingShip(travelingShip)
+	}
+}
 
+func (sector *Sector) AddExistingShip(ship ship.Ship){
+	sector.players = append(sector.players, ship)
 }
 
 func (sector *Sector) AddNewShip(shipName string) string {
@@ -42,8 +58,15 @@ func (sector *Sector) AddNewShip(shipName string) string {
 	return newShip.GetName()
 }
 
-func (sector *Sector) RemoveShip(){
-
+func (sector *Sector) RemoveShip(ship ship.Ship){
+	shipFound := false
+	for i:=0 ; i<len(sector.players) && !shipFound ; i++ {
+		shipFound = sector.players[i].GetName() == ship.GetName()
+		if shipFound {
+			sector.players[i] = sector.players[len(sector.players)-1]
+			sector.players = sector.players[:len(sector.players)-1]
+		}
+	}
 }
 
 func (sector *Sector) AmountOfShips(){
