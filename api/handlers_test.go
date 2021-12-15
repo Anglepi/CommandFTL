bea7cfe872d96527cd43d6fd3b51e7d93211dfca
Ángel Píñar Rivas <angle@correo.ugr.es>
2021-12-15 19:56:58 +0100

    closes #15 ADD test about forbidding duplicate ship names

--- api/handlers_test.go
+++ api/handlers_test.go
@@ -13,6 +13,7 @@ import (
 var server *Server = CreateServer()
 
 const StatusOK string = "OK"
+const StatusERR string = "ERROR"
 
 
 func executeRequest(request *http.Request) *httptest.ResponseRecorder{
@@ -35,4 +36,21 @@ func TestCreateNewPlayer(t *testing.T){
 
 	assert.Equal(t, data["status"], StatusOK)
 	assert.Equal(t, data["msg"], "SCT0/testShip")
+}
+
+func TestNewPlayerNameAlreadyChosen(t *testing.T){
+	req, _ := http.NewRequest("POST", "/newGame", bytes.NewBuffer([]byte(`{ "shipName": "testShip"}`)))
+	req2, _ := http.NewRequest("POST", "/newGame", bytes.NewBuffer([]byte(`{ "shipName": "testShip"}`)))
+	
+	executeRequest(req)
+	response := executeRequest(req2)
+
+	assert.Equal(t, http.StatusConflict, response.Code)
+	assert.Equal(t, "application/json", response.HeaderMap.Get("Content-Type"))
+
+	var data map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &data)
+
+	assert.Equal(t, data["status"], StatusERR)
+	assert.Equal(t, data["msg"], "A ship already exists with that name")
 }
